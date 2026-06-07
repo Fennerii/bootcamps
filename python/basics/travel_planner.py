5dd1dcc5f7ec4800d0e1e355daacd218b64af973
fennerii <jonathanfennerii@gmail.com>
2026-06-06 23:00:08 -0400

    built travel planner

---
+++ python/basics/travel_planner.py
@@ -0,0 +1,25 @@
+distance_mi = 0
+is_raining = False
+has_bike = True
+has_car = False
+has_ride_share_app = True
+
+if not distance_mi:
+    print(False)
+
+elif distance_mi <= 1 and is_raining == False:
+    print (True)
+elif distance_mi <=1:
+    print(False)
+
+elif distance_mi >= 1 and distance_mi <= 6:
+    if has_bike == True and is_raining == False:
+        print(True)
+    else:
+        print(False)
+
+elif distance_mi > 6:
+    if has_car or has_ride_share_app == True:
+        print(True)
+    else:
+        print(False)
