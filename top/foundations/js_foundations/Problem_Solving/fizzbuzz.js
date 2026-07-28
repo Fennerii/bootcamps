9df3f82f954423a875fea5146c290b715ef3e277
fennerii <jonathanfennerii@gmail.com>
2026-07-28 19:19:41 -0400

    fizz buzz

---
+++ top/foundations/js_foundations/Problem_Solving/fizzbuzz.js
@@ -0,0 +1,15 @@
+let n = 15;
+
+    for(i = 0; i <= 15; i++)
+    {
+        if(i % 3 == 0 && i % 5 == 0){
+            console.log("Fizz Buzz")
+        } else if (i % 3 === 0){
+            console.log("Fizz")
+        } else if (i % 5 === 0){
+            console.log("Buzz")
+        } else {
+            console.log(i);
+        }
+    }
+
